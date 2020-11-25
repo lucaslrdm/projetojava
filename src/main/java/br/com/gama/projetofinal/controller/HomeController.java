d1a5bf9bd8a0e0146b7291578b96957ecd8d300c
lucas menezes <lucas-rdm@hotmail.com>
2020-11-25 16:58:28 -0300

    HomeController.java

--- src/main/java/br/com/gama/projetofinal/controller/HomeController.java
+++ src/main/java/br/com/gama/projetofinal/controller/HomeController.java
@@ -1,9 +1,12 @@
-package br.com.agendamento.controller;
+package br.com.gama.projetofinal.controller;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
 
 @RestController
 @CrossOrigin ("*")
