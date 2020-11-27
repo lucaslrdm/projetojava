d6fc3505ecf0869f9a7261ab0bd32af6a3f4e39b
lucaslrdm <74377755+lucaslrdm@users.noreply.github.com>
2020-11-27 18:25:18 -0300

    alteracao - filtro ordenado

--- src/main/java/br/com/gama/projetofinal/controller/AgendamentoController.java
+++ src/main/java/br/com/gama/projetofinal/controller/AgendamentoController.java
@@ -24,7 +24,7 @@ public class AgendamentoController {
 	
 	@PostMapping("/relatorioporagencia")
 	public ResponseEntity<List<Agendamento>> getAgendamentoAgencia(@RequestBody Agendamento objeto){
-		List<Agendamento> lista = dao.findByAgenciaId(objeto.getAgencia().getId());
+		List<Agendamento> lista = dao.findByAgenciaIdOrderByDataagendamentoAscHoraagendamentoAsc(objeto.getAgencia().getId());
 		if(lista.size()==0) return ResponseEntity.status(404).build();
 		return ResponseEntity.ok(lista);
 		
@@ -32,7 +32,7 @@ public class AgendamentoController {
 	
 	@PostMapping("/relatoriopordata")
 	public ResponseEntity<List<Agendamento>> getAgendamentoData(@RequestBody Agendamento objeto){
-		List<Agendamento> lista = dao.findByDataagendamento(objeto.getDataagendamento());
+		List<Agendamento> lista = dao.findByDataagendamentoOrderByDataagendamentoAscHoraagendamentoAsc(objeto.getDataagendamento());
 		if(lista.size()==0) return ResponseEntity.status(404).build();
 		return ResponseEntity.ok(lista);
 		
@@ -40,14 +40,14 @@ public class AgendamentoController {
 	
 	@PostMapping("/relatorioporcliente")
 	public ResponseEntity<List<Agendamento>> getAgendamentoCliente(@RequestBody Agendamento objeto){
-		List<Agendamento> lista = dao.findByNomecli(objeto.getNomecli());
+		List<Agendamento> lista = dao.findByNomecliOrderByDataagendamentoAscHoraagendamentoAsc(objeto.getNomecli());
 		if(lista.size()==0) return ResponseEntity.status(404).build();
 		return ResponseEntity.ok(lista);
 	}
 	
 	@PostMapping("/relatorioporagenciadata")
 	public ResponseEntity<List<Agendamento>> getAgendamentoAgenciaData(@RequestBody Agendamento objeto){
-		List<Agendamento> lista = dao.findByAgenciaIdAndDataagendamento(objeto.getAgencia().getId(),objeto.getDataagendamento());
+		List<Agendamento> lista = dao.findByAgenciaIdAndDataagendamentoOrderByDataagendamentoAscHoraagendamentoAsc(objeto.getAgencia().getId(),objeto.getDataagendamento());
 		if(lista.size()==0) return ResponseEntity.status(404).build();
 		return ResponseEntity.ok(lista);
 		
@@ -55,7 +55,7 @@ public class AgendamentoController {
 	
 	@PostMapping("/relatorioporagenciacliente")
 	public ResponseEntity<List<Agendamento>> getAgendamentoAgenciaCliente(@RequestBody Agendamento objeto){
-		List<Agendamento> lista = dao.findByAgenciaIdAndNomecli(objeto.getAgencia().getId(),objeto.getNomecli());
+		List<Agendamento> lista = dao.findByAgenciaIdAndNomecliOrderByDataagendamentoAscHoraagendamentoAsc(objeto.getAgencia().getId(),objeto.getNomecli());
 		if(lista.size()==0) return ResponseEntity.status(404).build();
 		return ResponseEntity.ok(lista);
 		
@@ -63,7 +63,7 @@ public class AgendamentoController {
 	
 	@PostMapping("/relatorioporclientedata")
 	public ResponseEntity<List<Agendamento>> getAgendamentoClienteData(@RequestBody Agendamento objeto){
-		List<Agendamento> lista = dao.findByNomecliAndDataagendamento(objeto.getNomecli(),objeto.getDataagendamento());
+		List<Agendamento> lista = dao.findByNomecliAndDataagendamentoOrderByDataagendamentoAscHoraagendamentoAsc(objeto.getNomecli(),objeto.getDataagendamento());
 		if(lista.size()==0) return ResponseEntity.status(404).build();
 		return ResponseEntity.ok(lista);
 		
@@ -71,17 +71,17 @@ public class AgendamentoController {
 	
 	@PostMapping("/relatorioporagenciaclientedata")
 	public ResponseEntity<List<Agendamento>> getAgendamentoAgenciaClienteData(@RequestBody Agendamento objeto){
-		List<Agendamento> lista = dao.findByAgenciaIdAndNomecliAndDataagendamento(objeto.getAgencia().getId(),objeto.getNomecli(),objeto.getDataagendamento());
+		List<Agendamento> lista = dao.findByAgenciaIdAndNomecliAndDataagendamentoOrderByDataagendamentoAscHoraagendamentoAsc(objeto.getAgencia().getId(),objeto.getNomecli(),objeto.getDataagendamento());
 		if(lista.size()==0) return ResponseEntity.status(404).build();
 		return ResponseEntity.ok(lista);
 		
 	}
 	
 	@PostMapping("/validaagendamento")
-	public ResponseEntity<String> getHoraAgendamento(@RequestBody Agendamento objeto){
-		List<Agendamento> lista = dao.findByHoraagendamento(objeto.getHoraagendamento());
+	public ResponseEntity<List<Agendamento>> getHoraAgendamento(@RequestBody Agendamento objeto){
+		List<Agendamento> lista = dao.findByHoraagendamentoAndDataagendamentoAndAgenciaId(objeto.getHoraagendamento(), objeto.getDataagendamento(), objeto.getAgencia().getId());
 		if(lista.size()>2) return ResponseEntity.status(404).build();
-		return ResponseEntity.ok("lotado");
+		return ResponseEntity.ok(lista);
 		
 	}
 	
@@ -96,11 +96,19 @@ public class AgendamentoController {
 		}
 	}
 	
-	@GetMapping("/clientes")
+	@GetMapping("/agendamentos")
 	public ResponseEntity<List<Agendamento>> getAll(){
+		List<Agendamento> lista = (List<Agendamento>) dao.findAllByOrderByDataagendamentoAscHoraagendamentoAsc();
+		if(lista.size()==0) return ResponseEntity.status(404).build();
+		return ResponseEntity.ok(lista);
+	}
+
+	@GetMapping("/clientes")
+	public ResponseEntity<List<Agendamento>> getAllClientes(){
 		List<Agendamento> lista = (List<Agendamento>) dao.findAll();
 		if(lista.size()==0) return ResponseEntity.status(404).build();
 		return ResponseEntity.ok(lista);
 	}
+
 
 }
