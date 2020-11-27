d8f9a96627ba7dc79da248bb93c83668c25c14bf
lucaslrdm <74377755+lucaslrdm@users.noreply.github.com>
2020-11-27 18:27:05 -0300

    alteracao - funcao de ordenacao

--- src/main/java/br/com/gama/projetofinal/dao/AgendamentoDAO.java
+++ src/main/java/br/com/gama/projetofinal/dao/AgendamentoDAO.java
@@ -9,21 +9,22 @@ import br.com.gama.projetofinal.model.Agendamento;
 
 public interface AgendamentoDAO extends CrudRepository<Agendamento, Integer>{
 	
-	public List<Agendamento> findByAgenciaId(int id);
+	public List<Agendamento> findByAgenciaIdOrderByDataagendamentoAscHoraagendamentoAsc(int id);
 	
-	public List<Agendamento> findByDataagendamento(Date dataagendamento);
+	public List<Agendamento> findByDataagendamentoOrderByDataagendamentoAscHoraagendamentoAsc(Date dataagendamento);
 	
-	public List<Agendamento> findByNomecli(String nomecli);
+	public List<Agendamento> findByNomecliOrderByDataagendamentoAscHoraagendamentoAsc(String nomecli);
 	
-	public List<Agendamento> findByAgenciaIdAndDataagendamento(int id, Date dataagendamento);
+	public List<Agendamento> findByAgenciaIdAndDataagendamentoOrderByDataagendamentoAscHoraagendamentoAsc(int id, Date dataagendamento);
 	
-	public List<Agendamento> findByAgenciaIdAndNomecli(int id, String nomecli);
+	public List<Agendamento> findByAgenciaIdAndNomecliOrderByDataagendamentoAscHoraagendamentoAsc(int id, String nomecli);
 	
-	public List<Agendamento> findByNomecliAndDataagendamento(String nomecli, Date dataagendamento);
+	public List<Agendamento> findByNomecliAndDataagendamentoOrderByDataagendamentoAscHoraagendamentoAsc(String nomecli, Date dataagendamento);
 	
-	public List<Agendamento> findByAgenciaIdAndNomecliAndDataagendamento(int id, String nomecli,Date dataagendamento);
+	public List<Agendamento> findByAgenciaIdAndNomecliAndDataagendamentoOrderByDataagendamentoAscHoraagendamentoAsc(int id, String nomecli,Date dataagendamento);
 	
-	public List<Agendamento> findByHoraagendamento(String horaagendamento);
+	public List<Agendamento> findByHoraagendamentoAndDataagendamentoAndAgenciaId(String horaagendamento,Date dataagendamento,int id);
 	
+	public List<Agendamento> findAllByOrderByDataagendamentoAscHoraagendamentoAsc();
 
 }
